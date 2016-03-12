78a1e65cf55bd84dffcc31a086576f9efb18efe2
Антон Кияткин <a.kiyatkin@itlf.ru>
2016-03-12 14:38:29 +0400

    first commit

---
+++ infra.php
@@ -0,0 +1,16 @@
+<?php
+use infrajs\event\Event;
+use infrajs\view\View;
+use infrajs\access\Access;
+use WyriHaximus\HtmlCompress;
+
+Event::one('Infrajs.onshow', function () {
+
+	if (Access::debug()) return;
+	$html = View::html();
+
+	$parser = HtmlCompress\Factory::construct();
+	$html=$parser->compress($html);
+
+	View::html($html, true);
+});
